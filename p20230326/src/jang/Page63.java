6db71cecb37312052b736381c416311d1a7db609
Class01 <Class01@DESKTOP-VLQD395>
2023-04-26 17:26:51 +0900

    510 511

---
+++ p20230326/src/jang/Page63.java
@@ -0,0 +1,11 @@
+package jang;
+
+public class Page63 {
+	public static void main(String[] args) {
+		int x=0, y=0;
+		x=-1;
+		y=100;
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
---
+++ p20230326/src/jang/Page64.java
@@ -0,0 +1,12 @@
+package jang;
+
+public class Page64 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("55 - 10 = 45");
+		System.out.println("2008 - 1999 = 9");
+	}
+
+}
